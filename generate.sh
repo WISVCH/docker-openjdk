f84a3000c528a95c4e82bb56d0c66da5ccc89b54
Mark Janssen <mark@praseodym.net>
2018-03-06 21:27:39 +0000

    Fix Datadog APM agent permissions

--- generate.sh
+++ generate.sh
@@ -10,5 +10,6 @@ ADD https://ch.tudelft.nl/certs/wisvch.crt /usr/local/share/ca-certificates/wisv
 RUN chmod 0644 /usr/local/share/ca-certificates/wisvch.crt && \\
     update-ca-certificates
 ADD https://search.maven.org/remote_content?g=com.datadoghq&a=dd-java-agent&v=LATEST /opt/datadog/dd-java-agent.jar
+RUN chmod 0644 /opt/datadog/dd-java-agent.jar
 EOF
 done
