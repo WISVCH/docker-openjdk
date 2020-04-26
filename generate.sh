7b11ae4ff2a3d53c87de62eeb793228bf426c88d
Mark Janssen <mark@praseodym.net>
2020-04-26 15:39:22 +0200

    Add Java 14 JDK image
    * Remove Java 10 because it has been EOL since September 2018
    * Use explicit `-buster` image because newer OpenJDK images are based on
      Oracle Linux by default.
    * There are no Java 14 JRE images upstream, so we cannot build those.

--- generate.sh
+++ generate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for tag in 8-jdk 8-jre 8-jre-slim 10-jdk 10-jre 10-jre-slim 11-jdk 11-jre 11-jre-slim; do
+for tag in 8-jdk 8-jre 8-jre-slim 11-jdk 11-jre 11-jre-slim 14-jdk; do
 	echo $tag
 	mkdir -p $tag
 	cat > $tag/Dockerfile <<EOF
-FROM openjdk:$tag
+FROM openjdk:$tag-buster
 
 ADD https://ch.tudelft.nl/certs/wisvch.crt /usr/local/share/ca-certificates/wisvch.crt
 RUN chmod 0644 /usr/local/share/ca-certificates/wisvch.crt && \\
